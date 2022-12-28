8b841622fadb2e2a3c4d2f384f774bd6a5189a5d
Your Name <you@example.com>
2022-12-28 11:07:49 +0700

    update

---
+++ Lecture02DataTypes/29ex1.js
@@ -0,0 +1,21 @@
+    // Function to Find the Parameter of Triangle
+    function perimeterRectangle(a, b) {
+        let perimeter = 2 * (a + b);
+        return perimeter;
+    }
+
+    function perimeterRectangle(c, d) {
+        let perimeter = 2 * (c + d);
+        return perimeter;
+    }
+ 
+    // Driver program
+    let a = 6;
+    let b = 7;
+
+    // Driver program
+    let c = 20;
+    let d = 10;
+
+    console.log("Perimeter = " + perimeterRectangle(a, b));
+    console.log("Perimeter = " + perimeterRectangle(c, d));
