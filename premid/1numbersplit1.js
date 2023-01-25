50dd8a6269d99143b27bf764acde1ab27e545891
Your Name <you@example.com>
2023-01-25 11:20:19 +0700

    update

--- premid/1numbersplit1.js
+++ premid/1numbersplit1.js
@@ -1,14 +1,21 @@
-
-/*numberSplit(4) -> [2,2]
+/*
+numberSplit(4) -> [2,2]
 numberSplit(10) -> [5,5]
 numberSplit(11) -> [5,6]
-numberSplit(-9) -> [-5,-4] */
+numberSplit(-9) -> [-5,-4] 
+*/
 
 function numberSplit(n) {
-  if (n < 0) {
-      return [-Math.ceil(n/2), -Math.floor(n/2)];
+  if (n % 2 === 0) {
+      return [n/2, n/2];
   } else {
-      return [Math.floor(n/2), Math.ceil(n/2)];
+      let first = Math.floor(n/2);
+      let second = first + 1;
+      if (n < 0) {
+          first = -first;
+          second = -second;
+      }
+      return [first, second];
   }
 }
 
--- premid/2Highestdigit.js
+++ premid/2Highestdigit.js
@@ -7,3 +7,6 @@ highestDigit(377401) -> 7
 function highestDigit(num) {
     return Math.max(...num.toString().split(""))
 }
+console.log(highestDigit(379))
+console.log(highestDigit(2))
+console.log(highestDigit(377401))
